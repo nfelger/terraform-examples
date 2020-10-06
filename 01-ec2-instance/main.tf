3b21d1001b252940a2d2496c3c4f50f17d669517
Niko Felger <niko.felger@gmail.com>
2020-10-06 09:59:35 +0200

    Update main.tf

--- 01-ec2-instance/main.tf
+++ 01-ec2-instance/main.tf
@@ -16,10 +16,22 @@ data "aws_ami" "amazon-linux-2" {
   }
 }
 
+resource "aws_security_group" "default" {
+  name = "terraform-example"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "web" {
   instance_type   = "t2.micro"
   ami             = data.aws_ami.amazon-linux-2.id
   key_name        = var.keypair_name
+  security_groups = [aws_security_group.default.name]
 }
 
 output "public_ip" {
